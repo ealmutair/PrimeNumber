6ebfca8a49e01a90bfb21f90d667e0ec35fe1f46
EssamAlmutair <EssamAlmutair@Essams-MacBook-Pro-2.local>
2017-05-24 08:16:53 -0400

    testing

--- PrimeNumber/src/Generator.java
+++ PrimeNumber/src/Generator.java
@@ -1,22 +1,63 @@
+import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * @decs this class will generate specific range of numbers based on the user input,
+ * then find all the prime numbers in this range.
+ * Generator implement the inteface PrimeNumberGenerator methods(generate(),isPrime()). 
+ * @author EssamAlmutair
+ * @date May 24/17 
+ *
+ */
 
 public class Generator implements PrimeNumberGenerator {
 
 	//Implementation of the interface method
 	public boolean isPrime(int value) {
-		
-		return false;
-	
-	}
+		//skip 0,1,2 and even numbers>2
+		 if ( value > 2 && value%2 == 0 ) {
+		      //  System.out.println(value + " is not prime");
+		        return false;
+		    }
+		 
+		    int temp = (int)Math.sqrt(value) + 1;
+		    
+		    for(int i = 3; i < temp; i+=2){
+		        if(value % i == 0){
+		        //    System.out.println(value + " is not prime");
+		            return false;
+		        }
+		    }
+		    //System.out.println(value + " is prime");
+		    return true; 
+	}//end of method
 
-	//Implementation of the interface method
+	//Implementation of the interface method to generate numbers
 	public List<Integer> generate(int startingValue, int endingValue) {
 		
-		//Normal range (1,2,3,,..)
+		// list to save all the prime numbers
+		List<Integer>list= new ArrayList<Integer>();
 		
-		
-		//inverse range(10,9,8,..)
-		return null;
+		boolean value= true;
+		//for inverse range
+		if(startingValue>= endingValue){
+			
+			for(int i=startingValue;i>=endingValue;i--){
+				value = isPrime(i);// call isPrime to check if number is prime 
+				if(value==true){
+					list.add(i);
+				}// end if
+			}//end for loop
+			
+		}else{//for normal range
+					for(int i=startingValue;i<=endingValue;i++){
+				value =isPrime(i);// call isPrime to check if number is prime
+				if(value==true){
+					list.add(i);
+				}//end if 
+			}//end of for
+		}//end else
+		return list;
 	
-	}
-}
+	}//end method
+}//end of class
--- PrimeNumber/src/Main.java
+++ PrimeNumber/src/Main.java
@@ -1,9 +1,12 @@
-/*
- * Essam Almutair
- * May 24
- */
-import java.util.Scanner;
 
+import java.util.List;
+import java.util.Scanner;
+/**
+ * @decs This is the main class. It handle the interval range and 
+ * print out the prime numbers  
+ * @author EssamAlmutair
+ * @date May 24/17 
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -12,16 +15,19 @@ public class Main {
 		Generator generator = new Generator(); 
 		
 		
-		System.out.println("please enter numbers");
+		System.out.println("please enter first number in the range");
 		Scanner scan = new Scanner(System.in);
-		// Read first and last number in the interval 
+		
+		// Read first number in the interval 
 		int start_val = scan.nextInt();
+		System.out.println("please enter last number in the range");
+		// Read last number in the interval
 		int end_val = scan.nextInt();
 	
 		// Generate all numbers between start and end and get back primary number only
-		generator.generate(start_val, end_val);
+		List<Integer>list=generator.generate(start_val, end_val);
 		//Testing 
-		//System.out.println();
+		System.out.println("prime numbers in the range between "+start_val+" and "+end_val +" are "+list);
 		
 	}
 
